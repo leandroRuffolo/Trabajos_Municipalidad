f494ea51b3dd6154456fb94359dba63d0a7b8a82
leandroRuffolo <89546346+leandroRuffolo@users.noreply.github.com>
2021-11-12 13:36:07 -0300

    Add files via upload

---
+++ js/javaScript.js
@@ -0,0 +1,18 @@
+function InfoAutor(){
+  alert("Nombre: Leandro Nahuel Ruffolo Email: Leanruffolo@gmail.com Wpp: 91138501622")
+}
+  function inclusivo(){
+document.body.style.background = "#071523 ";
+document.getElementById("parrafo-5").style.color = "#E8E9EA ";
+document.getElementById("nav-2").style.textDecoration = "underline overline";
+document.getElementById("nav-2").style.textDecorationColor = "#447D7E ";
+document.getElementById("nav-2").style.color = "#E8E9EA ";
+document.getElementById("enlaces-1").style.background = "#5A734D  ";
+document.getElementById("enlace-milanesa").style.color = "#E8E9EA ";
+document.getElementById("pastel").style.color = "#E8E9EA ";
+document.getElementById("sandwich").style.color = "#E8E9EA ";
+document.getElementById("vitel").style.color = "#E8E9EA ";
+document.getElementById("section-1").style.background = "#1B2634 ";
+document.getElementById("aside-5").style.background = "#1B2634 ";
+
+}
